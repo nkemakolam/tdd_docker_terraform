4be5c073b0b2334f9c0b5d0c67992bc353d8bbdb
nkemakolam <nkemakolamnnadi3@gmail.com>
2019-10-27 07:10:53 +0100

    adding paostgress and configuration

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,34 @@
+# pull official base image
 FROM python:3.7.4-alpine
 
 # set working directory
 RUN mkdir -p /usr/src/app
-WORKDIR /usr/src/app
+
+
+# new
+# install dependencies
+RUN apk update && \
+    apk add --virtual build-deps gcc python-dev musl-dev && \
+    apk add postgresql-dev && \
+    apk add netcat-openbsd
 
 # set environment varibles
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
+WORKDIR /usr/src/app
 # add and install requirements
-COPY ./requirements.txt .
+COPY ./requirements.txt /usr/src/app/requirements.txt
 RUN pip install -r requirements.txt
 
-# add app
-COPY . .
 
+# new
+# add entrypoint.sh
+COPY ./entrypoint.sh /usr/src/app/entrypoint.sh
+RUN chmod +x /usr/src/app/entrypoint.sh
+
+# add app
+# add app
+COPY . /usr/src/app
 # run server
-CMD python manage.py run -h 0.0.0.0
+#CMD python manage.py run -h 0.0.0.0
--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,8 @@
+import os
+import sys  # new
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy  # new
 
 
 # instantiate the app
@@ -8,7 +11,25 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')  # new
+app_settings = os.getenv('APP_SETTINGS')  # new
+app.config.from_object(app_settings)      # new
+print(app.config, file=sys.stderr)
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer,primary_key=True,autoincremet=True)
+    username = db.Column(db.string(128),nullable=False)
+    email=db.Column(db.string(128),nullable=False)
+    active = db.Column(db.Boolean(),default=True,nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+       
+
+
 
 
 class Ping(Resource):

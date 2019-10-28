f0fed7ab6b9c53bbc5d8d598863efd19c736d303
nkemakolam <nkemakolamnnadi3@gmail.com>
2019-10-28 01:50:46 +0100

    updating manage.py

--- manage.py
+++ manage.py
@@ -1,10 +1,19 @@
 
+from project import app, db  # new
 from flask.cli import FlaskGroup
 
 from project import app
 
 
 cli = FlaskGroup(app)
+
+# new
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
 
 
 if __name__ == '__main__':
